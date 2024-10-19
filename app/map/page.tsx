80c7e59555a1ecc8effb132d5937de333c616f5f
Alemzhan-A <alimzanalimzag@gmail.com>
2024-10-19 16:50:53 +0500

    lesgo

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import * as React from 'react';
-import { useEffect } from 'react';
+import { Button } from "@/components/ui/button" 
+import { useEffect, useState } from 'react';
 import Map from 'react-map-gl';
+import { Zap } from "lucide-react"
+import Image from 'next/image';
 
 const token = `${process.env.NEXT_PUBLIC_MAP_BOX}`;
 
 function MapPage() {
+  const [energy] = useState(100)
+  const [coins] = useState(500)
   useEffect(() => {
     // Disable scrolling on mount
     document.body.style.overflow = 'hidden';
@@ -18,7 +23,21 @@ function MapPage() {
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground dark">
       <header className="p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Knowledge drops</h1>
+        <h1 className="text-2xl font-bold">Knowledge Drops</h1>
+        <div className="flex space-x-2 items-center">
+    <div className=" mt-3 flex items-center">
+      <Button variant="outline" size="default">
+      <Image src="/coin.png" alt="Coin" width={24} height={24} className="mr-2" />
+        {coins}
+      </Button>
+    </div>
+    <div className=" mt-3 flex items-center">
+    <Button variant="outline" size="default">
+      <Zap className="w-4 h-4 mr-2" />
+      {energy}
+    </Button>
+    </div>
+  </div>
       </header>
       <main className="flex-grow">
       <Map
