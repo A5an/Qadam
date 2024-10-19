08eee172ddb56c021243da917a39071f9212fcd9
Alemzhan-A <alimzanalimzag@gmail.com>
2024-10-19 16:32:46 +0500

    map fix

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 import * as React from 'react';
+import { useEffect } from 'react';
 import Map from 'react-map-gl';
 
 const token = `${process.env.NEXT_PUBLIC_MAP_BOX}`;
 
 function MapPage() {
+  useEffect(() => {
+    // Disable scrolling on mount
+    document.body.style.overflow = 'hidden';
+
+    // Re-enable scrolling on unmount
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground dark">
       <header className="p-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Ваше обучение</h1>
+        <h1 className="text-2xl font-bold">Knowledge drops</h1>
       </header>
       <main className="flex-grow">
       <Map
